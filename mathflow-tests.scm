323af5850a6f93b08a46ed4189437d3e6f3cad70
GG2R10 <herc2435@gmail.com>
2026-06-30 13:50:12 -0500

    Reorganizado test de objetos al archivo de tests

--- mathflow-tests.scm
+++ mathflow-tests.scm
@@ -431,3 +431,36 @@ pruebaalcanceambientes2();
 print(x)
 
 end")
+
+; simulacion de objetos por medio de funciones con estado
+(run "begin
+func Persona(nomb, ed){
+    var edad = ed;
+    var nombre = nomb;
+
+    func getNombre() {
+        return nombre
+    }; 
+
+    func getEdad() {
+        return edad
+    };
+
+    func setEdad(nuevaEdad) {
+        edad = nuevaEdad
+    };
+
+    func setNombre(nuevoNombre) {
+        nombre = nuevoNombre
+    }
+
+    return {\"getNombre\": getNombre, \"getEdad\": getEdad, \"setEdad\": setEdad, \"setNombre\": setNombre}
+};
+
+var p = Persona(\"Juan\", 30);
+
+print((ref-diccionario(p, \"getNombre\"))());
+(ref-diccionario(p, \"setNombre\"))(\"Peter\");
+print((ref-diccionario(p, \"getNombre\"))())
+
+end")
